73dccafbffbaa1587d123a2758f9949f9047ccef
GTT <1092082524@qq.com>
2023-07-31 21:04:04 +0800

    2023_7_31

---
+++ test_7_31/test.c
@@ -0,0 +1,75 @@
+#include<stdio.h>  
+
+//#include<math.h>
+//void my_strcpy(char*dest,char*src)
+//{
+//	while('\0'!=*src)
+//	{
+//	*dest=*src;
+//	 src++;
+//	 dest++;
+//	}
+//	*dest=*src;
+//}
+// int main()
+// {
+//	 char arr1[]="###########";
+//	 char arr2[] ="bit";
+//	 my_strcpy(arr1,arr2);
+//
+//	 printf("%s\n",arr1);
+//	 return 0;
+//
+// }
+
+//int main()
+//{
+//	int i=0;
+//	int arr[10]={1,2,3,4,5,6,7,8,9,10};
+//	for(i=0;i<=9;i++)  // i<=10    ËÀÑ­»·£¨Ô½½ç£©
+//	{ 
+//		printf("hehe\n");
+//		arr[i]=0;
+//	}
+//	return 0;
+//
+//}
+
+
+
+ ///*int main()
+ //{
+	// int i =0;
+	// int sum =0;
+	// int n=0;
+	// int ret =1;
+	// scanf("%d",&n);
+	// for(i=1;i<=n;i++)
+	// {
+	//	 int j=0;
+	//	 ret =1;
+	//	 for(j=1;j<=i;j++)
+	//	 {
+	//		  ret *=j;
+	//	 }
+	//	sum+= ret ;
+	// }
+ //     printf("%d ",sum);
+	// return 0; 
+
+ //}*/
+//int main()
+//{
+//	int i =0;
+//	for(i=0;i<=100;i++)
+//	{
+//		printf("%d ",i);
+//	}
+//	for(i=0;i<=100;i++)
+//	{
+// 	    printf("%d ",10-i);
+//	}
+//	return 0;
+//
+//} #include<stdio.h>
+
