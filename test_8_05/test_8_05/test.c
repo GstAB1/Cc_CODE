88fd6284953c056c1c3fc6a10b0044c818e04260
GTT <1092082524@qq.com>
2023-08-06 19:50:24 +0800

    测试

---
+++ test_8_05/test_8_05/test.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	int a[4][4];
+//	int b[4][4];
+//	int i=0;
+//	int j=0;
+//	printf("input 16 numbers:");
+//	for(i=0;i<=4;i++)
+//		for(j=0;j<4;j++)
+//		{
+//			scanf("%d",&a[i][j]);
+//			b[3-j][i]=a[i][j];
+//		}
+//		printf("array b:\n");
+//		for(i=0;i<4;i++)
+//		{
+//			for(j=0;j<4;j++)
+//				printf("%6d",b[i][j]);
+//			    printf("\n");
+//		}
+//	return 0;
+//}
+////CÓïÑÔ9*9³Ë·¨±í
+//int main()
+//{
+//	int i,j,result;
+//	for(i=1;i<=10;i++)
+//	{
+//		for(j=1;j<=10;j++)
+//		{
+//			  result=i*j;
+//		      printf("%d*%d=%-3d",i,j,result);
+//	    }printf("\n");
+//
+//	}return 0;
+//	
+//}
